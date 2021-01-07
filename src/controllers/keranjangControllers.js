f53dc7c09a7f1475c80804b1ec0140190ef6c453
install18 <install.delapanbelas@gmail.com>
2021-01-07 21:51:02 +0700

    Add & Remove Keranjang

--- src/controllers/keranjangControllers.js
+++ src/controllers/keranjangControllers.js
@@ -1,91 +1,140 @@
-import mongoose from 'mongoose';
-import { KeranjangSchema } from "../models/keranjangModel";
+import { Keranjang } from "../models/keranjangModel";
 
-const Keranjang = mongoose.model('Keranjang', KeranjangSchema);
+export const getAllCart = (req, res, next) => {
+  Keranjang.findOne({ idUser: "5fe48499f1445285b4a75db5" })
+    .then((cart) => res.status(200).json(cart))
+    .catch((err) => console.log(err));
+};
 
-export const getAllCart = (req, res) => {
-    Keranjang.find({ idUser: req.body.idUser },
-        (err, listProduct) => {
-            if (err) throw err;
-            res.status(200).json(listProduct);
-        });
-}
+//_id untuk test produk:
+//5fe4b3fc5ef112b297fa81a1
 
-//Belum di Test
 export const addToCart = (req, res) => {
-    try {
-        const newKeranjang = new Keranjang(req.body);
+  const product = req.body;
+  Keranjang.findOne({ idUser: "5fe48499f1445285b4a75db5" })
+    .then((cart) => {
+      const cartProductIndex = cart.listProduct.findIndex((cartProduct) => {
+        return cartProduct.idProduct.toString() === product._id.toString();
+      });
 
-        const findKeranjangUser = Keranjang.find({
-            idUser: req.body.idUser
-        }, (err, keranjang) => {
-            if (err) throw err;
-            if (!keranjang) {
-                newKeranjang.save((err) => {
-                    if (err) throw err;
-                    return res.status(200).json(keranjang, { message: 'Successfull Add To Keranjang' });
-                })
-            } else if (keranjang) {
-                return res.status(200).json({ message: 'Produk Sudah Ada Di Keranjang' });
-            }
+      let newQuantity = 1;
+      const updatedCartItems = [...cart.listProduct];
+
+      if (cartProductIndex >= 0) {
+        newQuantity = cart.listProduct[cartProductIndex].qtyProduct + 1;
+        updatedCartItems[cartProductIndex].qtyProduct = newQuantity;
+      } else {
+        updatedCartItems.push({
+          idProduct: product._id,
+          qtyProduct: newQuantity,
         });
-    } catch (error) {
-        return res.status(401).json(error);
-    }
-}
+      }
 
-//Belum di test
-export const addReduceQtyProduct = (req, res) => {
-    try {
-        if (req.body.status === "add") {
-            const data = Keranjang.findOneAndUpdate({
-                idUser: req.body.idUser,
-                "listProduct.idProduct": req.body.idProduct
-            }, {
-                $inc: {
-                    "listProduct.$.qtyProduct": +1
-                }
-            });
-            return res.status(200).json(data);
-        } else if (req.body.status === "reduce") {
-            const data = Keranjang.findOneAndUpdate({
-                idUser: req.body.idUser,
-                "listProduct.idProduct": req.body.idProduct
-            }, {
-                $inc: {
-                    "listProduct.$.qtyProduct": -1
-                }
-            });
-            return res.status(200).json(data);
-        }
-    } catch (error) {
-        return res.status(401).json(error);
-    }
-}
+      cart.listProduct = updatedCartItems;
+      res.status(200).json("Ok");
+      return cart.save();
+    })
+    .catch((err) => console.log(err));
+};
 
 export const removeFromCart = (req, res) => {
-    try {
-        const findKeranjangUser = Keranjang.findOneAndUpdate({
-            idUser: req.body.idUser
-        }, {
-            $pull: {
-                "listProduct.$.idProduct": req.body.idProduct
-            }
-        });
-        console.log("Find Keranjang ->" + findKeranjangUser);
-        return res.status(200).json(findKeranjangUser);
-    } catch (error) {
-        return res.status(401).json(error)
-    }
-}
+  const idProduct = req.body._id;
+  Keranjang.findOne({ idUser: "5fe48499f1445285b4a75db5" })
+    .then((cart) => {
+      const updatedCartItems = cart.listProduct.filter(
+        (item) => item.idProduct.toString() !== idProduct.toString()
+      );
 
-export const editQtyFromCart = (req, res) => {
-    Keranjang.updateOne({ idUser: req.params.idUser }, {
-        $set: { qty: req.body.qty }
-    }, (err) => {
-        if (err) {
-            res.send(err);
-        }
-        res.status(200).json({ message: 'Successfull Remove From Keranjang' });
-    });
-}
+      cart.listProduct = updatedCartItems;
+      res.status(200).json("Ok");
+      return cart.save();
+    })
+    .catch((err) => console.log(err));
+};
+
+// //Belum di Test
+// export const addToCart = (req, res) => {
+//   const newKeranjang = new Keranjang(req.body);
+//   Keranjang.find({ idUser: req.body.idUser })
+//     .then((keranjang) => {
+//       if (!keranjang) {
+//         newKeranjang.save((err) => {
+//           return res
+//             .status(200)
+//             .json(keranjang, { message: "Successfull Add To Keranjang" });
+//         });
+//       }
+//       return res.status(200).json({ message: "Produk Sudah Ada Di Keranjang" });
+//     })
+//     .catch((err) => console.error(err));
+// };
+
+// //Belum di test
+// export const addReduceQtyProduct = (req, res) => {
+//   try {
+//     if (req.body.status === "add") {
+//       const data = Keranjang.findOneAndUpdate(
+//         {
+//           idUser: req.body.idUser,
+//           "listProduct.idProduct": req.body.idProduct,
+//         },
+//         {
+//           $inc: {
+//             "listProduct.$.qtyProduct": +1,
+//           },
+//         }
+//       );
+//       return res.status(200).json(data);
+//     } else if (req.body.status === "reduce") {
+//       const data = Keranjang.findOneAndUpdate(
+//         {
+//           idUser: req.body.idUser,
+//           "listProduct.idProduct": req.body.idProduct,
+//         },
+//         {
+//           $inc: {
+//             "listProduct.$.qtyProduct": -1,
+//           },
+//         }
+//       );
+//       return res.status(200).json(data);
+//     }
+//   } catch (error) {
+//     return res.status(401).json(error);
+//   }
+// };
+
+// export const removeFromCart = (req, res) => {
+//   try {
+//     const findKeranjangUser = Keranjang.findOneAndUpdate(
+//       {
+//         idUser: req.body.idUser,
+//       },
+//       {
+//         $pull: {
+//           "listProduct.$.idProduct": req.body.idProduct,
+//         },
+//       }
+//     );
+//     console.log("Find Keranjang ->" + findKeranjangUser);
+//     return res.status(200).json(findKeranjangUser);
+//   } catch (error) {
+//     return res.status(401).json(error);
+//   }
+// };
+
+// export const editQtyFromCart = (req, res) => {
+//   Keranjang.updateOne(
+//     { idUser: req.params.idUser },
+//     {
+//       $set: { qty: req.body.qty },
+//     },
+//     (err) => {
+//       if (err) {
+//         res.send(err);
+//       }
+//       res.status(200).json({ message: "Successfull Remove From Keranjang" });
+//     }
+//   );
+// };
+
--- src/models/keranjangModel.js
+++ src/models/keranjangModel.js
@@ -1,20 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-export const KeranjangSchema = new Schema({
-    idUser: {
+const keranjangSchema = new Schema({
+  idUser: {
+    type: String,
+    required: true,
+  },
+  listProduct: [
+    {
+      idProduct: {
         type: String,
-        required: true
+        required: true,
+      },
+      qtyProduct: {
+        type: Number,
+        required: true,
+      },
     },
-    listProduct: [{
-        idProduct: {
-            type: String,
-            required: true,
-        },
-        qtyProduct: {
-            type: Number,
-            required: true
-        }
-    }],
+  ],
 });
+
+export const Keranjang = mongoose.model("Keranjang", keranjangSchema);
+
